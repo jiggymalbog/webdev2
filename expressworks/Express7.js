21532feb480ee88547ef01a1c816f6366313dfca
jiggymalbog <malbog.it111@gmail.com>
2020-02-20 18:27:44 +0800

    removed

--- expressworks/Express7.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.get('/search', function(req, res){
-    const query = req.query
-    res.send(query)
-})
-
-app.listen(process.argv[2])
